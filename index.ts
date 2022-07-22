318b0a994e08ba07d8b338d4ef10c5236c14bcdf
hassaanfarooq01 <hassaanfarooq71@gmail.com>
2022-07-22 16:05:53 +0500

    route changed

--- index.ts
+++ index.ts
@@ -38,7 +38,7 @@ const options = {
 
 const specs = swaggerJsDoc(options);
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.db = db;
 
