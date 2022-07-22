9968a21714edcc2888ac4b4fc6176c9aa981b334
hassaanfarooq01 <hassaanfarooq71@gmail.com>
2022-07-22 16:58:29 +0500

    access control

--- index.ts
+++ index.ts
@@ -30,6 +30,14 @@ const options = {
   apis: ["./routes/*.js"],
 };
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const specs = swaggerJsDoc(options);
 
 app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
